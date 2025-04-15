e1cad4c52e5e467cad33acb30038497a6ffe026a
Saher Hussain <saherxhussain@gmail.com>
2025-04-15 22:08:31 +0500

    Add files via upload

---
+++ work.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("imdb_top_5000_tv_shows.csv") #read the csv file 
+# print(df.columns) #to display the rows
+
+df.drop_duplicates(inplace=True)
+df.dropna(subset=["endYear"], inplace=True)
+df['endYear'] = df['endYear'].astype(int)
+df['duration'] = df['endYear'] - df['startYear']
+df['rating_100'] = df['averageRating'] * 10
+df['rating_category'] = pd.cut(df['averageRating'],
+                               bins=[0, 5, 7.5, 10],
+                               labels=['Low', 'Average', 'High'])
+df['votes_normalized'] = (df['numVotes'] - np.mean(df['numVotes'])) / np.std(df['numVotes'])
+dw_ready = df[['primaryTitle', 'startYear', 'endYear', 'duration',
+               'averageRating', 'rating_100', 'rating_category',
+               'numVotes', 'votes_normalized', 'genres', 'directors']]
+
+print(df.info())
+# print(dw_ready.describe(include='all'))
+# Save the engineered data to a new CSV
+# dw_ready.to_csv("imdb_dw_ready.csv", index=False)
