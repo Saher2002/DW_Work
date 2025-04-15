f7e4809f300cf95f51fc5b9b442081ec57a0fe31
Saher Hussain <saherxhussain@gmail.com>
2025-04-15 22:14:45 +0500

    Add files via upload

--- work.py
+++ work.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("imdb_top_5000_tv_shows.csv") #read the csv file 
-# print(df.columns) #to display the rows
+# print(df.columns) #display the rows
 
 df.drop_duplicates(inplace=True)
 df.dropna(subset=["endYear"], inplace=True)
@@ -17,7 +17,9 @@ dw_ready = df[['primaryTitle', 'startYear', 'endYear', 'duration',
                'averageRating', 'rating_100', 'rating_category',
                'numVotes', 'votes_normalized', 'genres', 'directors']]
 
-print(df.info())
+# print(df.info())
+print(df.columns) 
+print(dw_ready.columns) 
 # print(dw_ready.describe(include='all'))
 # Save the engineered data to a new CSV
-# dw_ready.to_csv("imdb_dw_ready.csv", index=False)
+dw_ready.to_csv("imdb_dw_ready.csv", index=False)
